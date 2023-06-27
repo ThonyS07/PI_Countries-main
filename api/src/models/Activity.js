3bfeba26e099b376cba9392ca0dd081b96aee43f
Thonys07 <thonysilva1993@gmail.com>
2023-06-26 23:50:57 -0500

    modelos BD

---
+++ api/src/models/Activity.js
@@ -0,0 +1,65 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+	// defino el modelo
+	sequelize.define(
+		"activity",
+        {
+            name: {
+                type: DataTypes.STRING,
+                set(value) {
+                    this.setDataValue("name", value.toUpperCase());
+                },
+            },
+
+            activityId: {
+				type: DataTypes.UUID,
+				defaultValue: DataTypes.UUIDV4,
+				allowNull: false,
+				primaryKey: true,
+			},
+
+			dificult: {
+				type: DataTypes.INTEGER,
+				validate: {
+					min: 1,
+					max: 5,
+					isEven(value) {
+						if (value < 1 || value > 5)
+							throw new Error("La dificultad debe ser un valor entre 1 y 5");
+					},
+				},
+			},
+
+			duration: {
+				type: DataTypes.INTEGER,
+				validate: {
+					min: 1,
+					max: 24,
+					isEven(value) {
+						if (value < 1 || value > 24)
+							throw new Error("La duración debe ser entre 1 hora y 24 horas");
+					},
+				},
+			},
+			season: {
+				type: DataTypes.ENUM(
+					"verano",
+					"primavera",
+					"invierno",
+					"otoño",
+					"todas"
+				),
+				set(value) {
+					this.setDataValue("season", value.toLowerCase());
+				},
+			},
+		},
+		{
+			timestamps: true,
+			createdAt: false,
+			updatedAt: "actualizado",
+		}
+	);
+};
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,43 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    countryId: {
+      type: DataTypes.STRING(3),
+      PRIMARY_KEY: true,
+      allowNull:false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    flagImg: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+
+  },{
+    timestamps: true,
+    createdAt: false,
+    updatedAt: 'actualizado'
   });
+
+ 
 };
