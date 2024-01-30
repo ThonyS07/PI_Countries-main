6bb8077444c8bb9f603966d116f74c943acbd3ab
Thony <thonysilva1993@gmail.com>
2024-01-30 14:50:22 -0500

    nueva actualizacion

--- api/index.js
+++ api/index.js
@@ -1,31 +1,13 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 const server = require("./src/app.js");
 const { conn, Country } = require("./src/db.js");
 const loadDb = require("./src/controllers/loadDb");
-
+const PORT = process.env.PORT || 3001;
 // Syncing all the models at once
 conn.sync({ force: false}).then(() => {
-	server.listen(process.env.PORT, () => {
+	server.listen(PORT, () => {
 		loadDb();
-		console.log("listening at PORT:" + process.env.PORT); // eslint-disable-line no-console
+		console.log("listening at PORT:" + PORT); // eslint-disable-line no-console
 	});
 });
 
--- api/src/db.js
+++ api/src/db.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+	`postgres://postgres:admin@localhost/countries`,
 	{
 		logging: false, // set to console.log to see the raw SQL queries
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
